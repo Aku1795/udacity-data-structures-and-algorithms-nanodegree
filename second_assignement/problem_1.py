b6d4cb7ad3cfb80ee8c087a05c7f744e5c734e06
Aku1795 <mtvc95@gmail.com>
2021-08-30 06:28:20 +0200

    first working version of LRU cache

--- second_assignement/problem_1.py
+++ second_assignement/problem_1.py
@@ -1,66 +1,62 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
+class Node(object):
+    def __init__(self, node_key, val):
+        self.node_key = node_key
+        self.val = val
         self.next = None
-        self.previous = None
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+        self.prev = None
         
-    def append(self, value):
-        if self.head is None:
-            self.head = Node(value)
-            self.tail = Node(value)
-            return
+class LRUCache:
+    def __init__(self, max_size):
+        self.head = Node('head', 'head')
+        self.tail = Node('tail', 'tail')
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.max_size = max_size
+        self.curr_size = 0
+        self.node_lookup = {}
         
-        node = self.tail
-        node.next = Node(value)
-        node.next.previous = node
-        self.tail = node.next
-    
-    def to_list(self):
-        _list = []
-        head = self.head
-        while head is not None:
-            _list.append(head.value)
-            head = head.next
-        return _list
+    def remove_node(self, node):
+        node_after = node.next
+        node_before = node.prev
+        node_before.next = node_after
+        node_after.prev = node_before      
+
+    def push_back(self, node):
+        prev_node_tail = self.tail.prev
+        prev_node_tail.next = node
+        node.prev = prev_node_tail
+        node.next = self.tail
+        self.tail.prev = node     
+
+    def get(self, node_key):     
+        if node_key not in self.node_lookup:
+            return -1
+        node = self.node_lookup[node_key]
+        node_val = node.val
+        self.remove_node(node)
+        self.push_back(node)
+        return node_val
         
-    def __iter__(self):
-        node = self.head
-        while node:
-            yield node.value
-            node = node.next
-            
-    def __repr__(self):
-        return str([v for v in self])
+    def set(self, node_key, new_val):
+        if node_key in self.node_lookup:
+            node = self.node_lookup[node_key]
+            node.val = new_val
+            self.remove_node(node)
+            self.push_back(node)   
+        else:
+            if self.curr_size >= self.max_size:
+                lru_node = self.head.next
+                del self.node_lookup[lru_node.node_key]
+                self.remove_node(lru_node)
+                self.curr_size -=1
+
+            new_node = Node(node_key, new_val)
+            self.node_lookup[node_key] = new_node
+            self.push_back(new_node)
+            self.curr_size += 1
 
 
-class LRU_Cache(object):
-    def __init__(self, capacity):
-        # Initialize class variables
-        self.capacity = capacity
-        self.cache = dict()
-        self.usage_rank = DoublyLinkedList()
-        self.num_elements = 0
-
-    def get(self, key):
-        # Retrieve item from provided key. Return -1 if nonexistent.
-        if key in self.cache:
-            
-            return self.cache[key]
-        return -1
-
-    def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
-        if self.num_elements == 0:
-            self.cache[key] = value
-            self.usage_rank.append(key)
-
-
-our_cache = LRU_Cache(5)
+our_cache = LRUCache(5)
 
 our_cache.set(1, 1)
 our_cache.set(2, 2)
@@ -75,6 +71,4 @@ our_cache.get(9)  # returns -1 because 9 is not present in the cache
 our_cache.set(5, 5)
 our_cache.set(6, 6)
 
-print(
-    our_cache.get(3)
-)  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+print(our_cache.get(3))  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
